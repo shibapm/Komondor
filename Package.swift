b55ada6a9a0f4027d3b56e3db68d6ed674201412
Franco Meloni <franco.meloni91@gmail.com>
2019-04-19 09:50:47 +0100

    Unhide dependencies

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         .package(url: "https://github.com/shibapm/PackageConfig", from: "0.10.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.1.0"),
         // Dev deps
-//        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.35.8"), // dev
-//        .package(url: "https://github.com/Realm/SwiftLint.git", from: "0.28.1"), // dev
-//        .package(url: "https://github.com/f-meloni/Rocket", from: "0.1.0"), // dev
+        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.35.8"), // dev
+        .package(url: "https://github.com/Realm/SwiftLint.git", from: "0.28.1"), // dev
+        .package(url: "https://github.com/f-meloni/Rocket", from: "0.1.0"), // dev
     ],
     targets: [
         .target(
